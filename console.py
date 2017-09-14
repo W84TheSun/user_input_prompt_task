658a280ee7d7fee7dbd9c6cef3032a4a33c26205
w84thesun-pc\w84thesun <wait4thesun@gmail.com>
2017-09-14 14:56:08 +0300

    First version
    Added first part of task.
    TODO: test for various inputs.

---
+++ console.py
@@ -0,0 +1,51 @@
+import operator
+import re
+
+def help():
+    return "Usage: N [word M]"
+
+
+def find_suitable_words(prompt, words):
+    matched = []
+    regex = re.compile("^" + prompt)
+    for word_pair in words:
+        match = regex.search(word[0])
+        if match:
+            matched.append(match.group(1))
+
+    if len(matched) > 0:
+        return "\n".join(matched)
+    else:
+        return "Nothing found."
+
+def main():
+    words_founded = input()
+    words_dictionary = {}
+    prompts = []
+    if 1 <= words_founded <= 10 ** 5:
+        for i in range(words_founded):
+            word, freq = input().split()
+            if len(word) > 15:
+                return "%s is longer than 15 letters!" % word
+            if word not in words_dictionary:
+                words_dictionary[word] = freq
+    words_prompted = input()
+    if 1 <= words_prompted <= 15000:
+        for i in range(words_prompted):
+            prompt = input()
+            if len(prompt) > 15:
+                return "%s is longer than 15 letters"
+            prompts.append(prompt)
+
+    words = sorted(words_dictionary.items(),
+                   key=operator.itemgetter(1),
+                   reverse=True)
+
+    for prompt in prompts:
+        results = find_suitable_words(prompt, words)
+        print(results)
+        print("")
+    return help()
+
+if __name__ == '__main__':
+    main()
